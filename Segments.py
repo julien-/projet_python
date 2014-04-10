c56244e580da65f8122d083dabb89dd9f3fe4ad3
damien57050 <collet.damien@hotmail.fr>
2014-04-10 15:06:44 +0200

    Modification Segments

--- Segments.py
+++ Segments.py
@@ -1,30 +1,15 @@
 from FormesSimples import FormesSimples
 
 class Segments(FormesSimples):
-
-   #numero du segment pour créer le nom de chaque forme afin de differencier
+    #numero du segment pour créer le nom de chaque forme afin de differencier
     numero = 0
     def __init__(self, nom, point1, point2, couleur):
         super(Segments,self).__init__(nom, point1, point2, couleur)
-  
-    def _get_point1(self):
-        return self._point1
-
-    def _set_point1(self, point1):
-        self._point1 = point1
-
-    point1 = property (_get_point1, _set_point1) 
+ 
+    def maj(self, p1, p2):       
+        super().maj(p1, p2)
     
-    def _get_point2(self):
-        return self._point2
-
-    def _set_point2(self, point2):
-        self._point2 = point2
-
-    point2 = property (_get_point2, _set_point2)
-    
-
+    def write(self):
+        super().write()
         
-    def write(self, p1, p2):
-        super().write(p1, p2)
         
