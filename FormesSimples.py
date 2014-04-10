01948c9ba4492965299f451cd8292d83a8a947c8
damien57050 <collet.damien@hotmail.fr>
2014-04-10 15:03:23 +0200

    Modification Formes Simples

--- FormesSimples.py
+++ FormesSimples.py
@@ -8,12 +8,14 @@ class FormesSimples(Formes):
         self._point1 = point1
         self._point2 = point2
         self._couleur = couleur  
+ 
     #_couleur    
     def _get_couleur(self):
         return self._couleur
 
     def _set_couleur(self, couleur):
         self._couleur = couleur
+        
     #_point1
     def _get_point1(self):
         return self._point1
@@ -21,6 +23,7 @@ class FormesSimples(Formes):
     def _set_point1(self, point):
         self._point1._set_x(point._get_x());
         self._point1._set_y(point._get_y());
+        
     #_point2
     def _get_point2(self):
         return self._point2
@@ -32,19 +35,24 @@ class FormesSimples(Formes):
     couleur = property (_get_couleur, _set_couleur)
     point1 = property (_get_point1, _set_point1)
     point2 = property (_get_point2, _set_point2)
-        
-    def write(self, p1, p2):
-        super().write(p1, p2)
+    
+    def maj(self, p1, p2):
+        super().maj()
         
         self._set_couleur(self.couleur);
         self._set_point1(Point(p1._get_x(), p1._get_y()));
         self._set_point2(Point(p2._get_x(), p2._get_y()));
-        #print("Couleur : " + self._couleur.__str__())
-        print("Point1 : " + self._point1._get_x().__str__() + "," + self._point1._get_y().__str__())
-        print("Point2 : " + self._point2._get_x().__str__() + "," + self._point2._get_y().__str__())
+        
+    def write(self):
+        super().write()
+        
+        print("Point1 : " + self._get_point1().x.__str__() + "," + self._get_point1().y.__str__())
+        print("Point2 : " + self._get_point2().x.__str__() + "," + self._get_point2().y.__str__())
     
     def translation(self, x, y):
         super().translation()
         #Point1
         self._point1 = Point(self._point1._get_x() + x, self._point1._get_y() + y)
         self._point2 = Point(self._point2._get_x() + x, self._point2._get_y() + y)
+    
+        
